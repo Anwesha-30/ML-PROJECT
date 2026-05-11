1e55a435c6e9834528f4cf3efe24175edee963ae
Anwesha-30 <anwesha.samaddar.cse.2024@tint.edu.in>
2026-05-12 01:40:39 +0530

    changes made some are

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages,setup
 from typing import List
+#imported from typing
 def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
